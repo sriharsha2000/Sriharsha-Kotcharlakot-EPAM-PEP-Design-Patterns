51a6218fca03495c426aa2a14c8a04c37127a8b2
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2020-03-10 09:47:19 +0530

    Update App.java

--- designpatterns/src/main/java/com/epampep/designpatterns/App.java
+++ designpatterns/src/main/java/com/epampep/designpatterns/App.java
@@ -1,13 +1,10 @@
 package com.epampep.designpatterns;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Singleton s = Singleton.getInstance();
+    	System.out.println("Singleton ID = " + s.getId());
+    	System.out.println("Singleton Name = " + s.getName());   	
     }
 }
