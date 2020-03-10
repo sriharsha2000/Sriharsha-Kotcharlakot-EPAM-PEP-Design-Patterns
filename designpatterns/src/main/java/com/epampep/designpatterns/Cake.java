03383dd7c64877c4670dad1d0556443feacd85cf
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2020-03-10 10:25:19 +0530

    Added template pattern implementation.

---
+++ designpatterns/src/main/java/com/epampep/designpatterns/Cake.java
@@ -0,0 +1,31 @@
+package com.epampep.designpatterns;
+
+public abstract class Cake implements SweetTemplate, Comparable<Cake> {
+	int sweetness = 0;
+	int weight = 0;
+	public int compareTo(Cake c)
+	{
+		return this.weight - c.weight;
+	}
+	public Cake(int sweet, int weight)
+	{
+		setSweetness(sweet);
+		setWeight(weight);
+	}
+	public void setSweetness(int s)
+	{
+		sweetness = s;
+	}
+	public void setWeight(int s)
+	{
+		weight = s;
+	}
+	public int getSweetness()
+	{
+		return sweetness;
+	}
+	public int getWeight()
+	{
+		return weight;
+	}
+}
